92d0301c9c7f240c7fc7675f7a42108e785ba69a
Luminita <hornoiluminita@github.com>
2019-02-09 17:05:18 +0200

    initial

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        System.out.println("Luminita");
+       int addition = 7+8 ;
+       System.out.println(”The addition of 7 an 8 is:”);
+     System.out.println(”addition= ” + addition);
+        int divide = 7/8 ;
+        System.out.println(”divide= ” + divide);
     }
+
 }
