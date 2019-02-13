7d57c8e1f465431e49f0ddbac8987b683508026e
Luminita <hornoiluminita@github.com>
2019-02-13 19:33:13 +0200

    initial

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,42 @@ public class Main {
 
         System.out.println("Hello World!");
         System.out.println("Luminita");
-        int x =7;
-        int y =8;
-       int addition = x + y ;
-       System.out.println(” The addition of ” + x + ” and ”+ y + ” is: ” + addition);
-       System.out.println(” x+y ”);
-        int divide = x:y ;
-        System.out.println(” x:y ”);
+        int x = 7;
+        int y = 8;
+        int addition = x + y;
+        System.out.println(" The addition of " + x + " and " + y + "is: " + addition);
+        System.out.println("x+y ");
+        int divide = x / y;
+        System.out.println(" x/y ");
+
+        int res = scadere(2, 3);
+        System.out.println(res);
+
+        int res2 = inmultire(2, 3);
+        System.out.println(res2);
+
+
+        int res3 = impartire(2, 3);
+        System.out.println(res3);
+    }
+
+    public static int scadere(int primulnumar, int aldoileanumar) {
+        int resultat = primulnumar - aldoileanumar;
+        return resultat;
+    }
+
+    public static int inmultire(int primulnumar, int aldoileanumar) {
+
+        int resultat2 = primulnumar * aldoileanumar;
+        return resultat2;
+    }
+
+
+    public static int impartire(int primulnumar, int aldoileanumar) {
+
+        int resultat3 = primulnumar / aldoileanumar;
+        return resultat3;
+
     }
 
 }
