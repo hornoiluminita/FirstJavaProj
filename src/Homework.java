295515b7b0cf1317f3909ea585f2c04164637be1
Luminita <hornoiluminita@github.com>
2019-03-03 12:24:56 +0200

    initial

--- src/Homework.java
+++ src/Homework.java
@@ -1,11 +1,11 @@
 public class Homework {
     import java.util.Scanner;
 
-            public static void main(String[] args) {
-                System.out.println("3\n" + "Hello World!");
-            }
+    public static void main(String[] args) {
+        System.out.println("3\n" + "Hello World!");
+    }
 
-            //
+    //
 //        public static void countnumber (); {
 //
 //
@@ -21,56 +21,69 @@ public class Homework {
 //            i++;
 //            System.out.println(rezultat);
 //        }
-            public static void sum ( int s){
+    public static void sum(int s) {
 
-                int l = 0;
-                while (l <= 100) {
-                    System.out.print("s");
-                }
-            }
-            public static void countnumber () {
-                for (int i = 10; i > 0; i--) {
-                    System.out.println("i = " + i);
-                }
-            }
+        int l = 0;
+        while (l <= 100) {
+            System.out.print("s");
+        }
+    }
 
-            public static void printPattern () {
+    public static void countnumber() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("i = " + i);
+        }
+    }
 
-                int i, j, k;
+    public static void printPattern() {
 
-                for (i = 1; i <= 7; i++) {
-                    for (j = 1; j <= i; ++j)
-                        System.out.print(j);
+        int i, j, k;
 
-                    for (k = 7 - i; k >= 1; k--)
-                        System.out.print("*");
+        for (i = 1; i <= 7; i++) {
+            for (j = 1; j <= i; ++j)
+                System.out.print(j);
 
-                    System.out.println("");
-                }
-            }
-            public static void sumandaverage () {
-                int num = 100, sum = 0;
+            for (k = 7 - i; k >= 1; k--)
+                System.out.print("*");
 
-                for (int i = 1; i <= num; ++i) {
-                    // sum = sum + i;
-                    sum += i;
-                }
+            System.out.println("");
+        }
+    }
 
-                System.out.println("Sum = " + sum);
-                System.out.println("Average is :" + sum / 50);
-            }
-    public static void sumaverage () {
+    public static void sumandaverage() {
+        int num = 100, sum = 0;
+
+        for (int i = 1; i <= num; ++i) {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average is :" + sum / 50);
+    }
+
+    public static void sumaverage() {
         int num = 100, sum = 0;
         int i = 1;
-        do{
+        do {
             System.out.println("Sum = " + sum);
-            i++;  sum += i;
+            i++;
+            sum += i;
             System.out.println("Average is :" + sum / 50);
-        } while ( i <= num);
+        } while (i <= num);
 
     }
 
+    public static void sumcount() {
+        int count = 0;
+        int num = 8899;
+        int i = 111;
+
+        for (i >= 8899) {
+            ++count;
+        }
     }
+}
 
 
 
